75a98c1e6c3262ae92508ba7e44999c992c50575
Siri Reddy <sirireddy.orange@gmail.com>
2024-07-15 13:05:09 +0530

    second commit

--- teacher_duty/settings.py
+++ teacher_duty/settings.py
@@ -118,10 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+'''
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static') #to make the path an absolute path
 ]
-
+'''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
